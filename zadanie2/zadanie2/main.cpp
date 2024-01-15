e147b3a6372752afd3e390275e77431cf8cf1ad4
x2k666 <69635125+x2k666@users.noreply.github.com>
2024-01-15 23:40:48 +1000

    Update zadanie 2

--- zadanie2/zadanie2/main.cpp
+++ zadanie2/zadanie2/main.cpp
@@ -1,38 +1,37 @@
-﻿#include "chunk.h"
+#include "chunk.h"
 #include <iostream>
 using namespace std;
-int main() 
-{
+int main() {
+    // Создаем объект ChunkList для тестирования
     fefu_laboratory_two::ChunkList<int, 3> chunkList;
 
-    // Вставка элементов
-    for (int i = 1; i <= 10; ++i) 
-        chunkList.insert(i);
-    
+    // Добавляем элементы
+    chunkList.insert(1);
+    chunkList.insert(2);
+    chunkList.insert(3);
 
-    // Вывод содержимого после вставки
-    cout << "ChunkList after insertions:" << endl;
-    for (const auto& chunk : chunkList) 
-    {
-        for (const auto& value : chunk.data) 
-            cout << value << " ";
-        
-        cout << endl;
+    // Выводим значения в каждом чанке
+    std::cout << "Values in Chunks:\n";
+    for (const auto& chunk : chunkList) {
+        for (const auto& value : chunk.data) {
+            std::cout << value << ' ';
+        }
+        std::cout << '\n';
     }
+    std::cout << std::endl;
 
-    // Удаление элементов
+    // Удаляем элемент
     chunkList.erase(2);
-    chunkList.erase(5);
-    chunkList.erase(8);
 
-    // Вывод содержимого после удалений
-    cout << "ChunkList after erasures:" << endl;
-    for (const auto& chunk : chunkList)
-    {
-        for (const auto& value : chunk.data) 
-            cout << value << " ";
-        
-        cout <<endl;
+    // Выводим значения в каждом чанке после удаления
+    std::cout << "Values in Chunks after Erasure:\n";
+    for (const auto& chunk : chunkList) {
+        for (const auto& value : chunk.data) {
+            std::cout << value << ' ';
+        }
+        std::cout << '\n';
     }
+    std::cout << std::endl;
 
+    return 0;
 }
